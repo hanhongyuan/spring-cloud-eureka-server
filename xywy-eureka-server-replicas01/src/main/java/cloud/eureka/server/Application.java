1e64eaf25353883e700f5a3b0b6ecb137927935d
yuliang <yuliang@xywy.com>
2016-08-11 09:33:42 +0800

    更新eureka的配置

--- xywy-eureka-server-replicas01/src/main/java/cloud/eureka/server/Application.java
+++ xywy-eureka-server-replicas01/src/main/java/cloud/eureka/server/Application.java
@@ -1,9 +1,13 @@
 package cloud.eureka.server;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
